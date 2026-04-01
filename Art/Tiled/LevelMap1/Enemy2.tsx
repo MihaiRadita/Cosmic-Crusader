025f9f557024525067bda44bee873e7020bf546d
MihaiRadita <mihairadita16@gmail.com>
2026-04-01 22:48:27 +0300

    Drone Enemy 1
    Added the sprite images of the second enemy of the game in the Enemies file. Also, added the the enemy sprite in the tiled for preparation. This will be the flying enemy of the Level 1

---
+++ Art/Tiled/LevelMap1/Enemy2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Enemy2" tilewidth="192" tileheight="100" tilecount="1" columns="1">
+ <image source="../../../Cosmic Crusader/Cosmic Crusader/Textures/EnemiesTextures/Enemy2Textures/IdleTextures/Enemy2Blaster/Angle0/Aim/Idle1.png" width="192" height="100"/>
+</tileset>
